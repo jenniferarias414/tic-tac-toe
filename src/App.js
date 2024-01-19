9eb3ceafac60b53e5f451009de74a37d02b93305
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-01-19 14:23:19 -0600

    project complete

--- src/App.js
+++ src/App.js
@@ -10,12 +10,34 @@ function App() {
   const handleClick = () => {
     setSquares(["", "", "", "", "", "", "", "", ""])
     setPlayer(true)
-}
+  }
+
+  const calculateWinner = (arr) => {
+    const lines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    for (let i=0; i < lines.length; i++){
+      console.log(lines[i])
+      const [a, b, c] = lines[i];
+      if (arr[a] &&
+        arr[a] === arr[b] &&
+        arr[a] === arr[c]){
+          return `${arr[a]} wins this one!`
+      }
+    } return 'Who will win?'
+  }
+
+  const myStyle = {
+    marginTop: '80px',
+    marginRight: '30px',
+    marginBottom: '20px',
+  };
 
   // const newVar = 'Props to you'
   return (
     <div className="App">
-      <button onClick={handleClick}>Reset</button>
+      <button style={myStyle} onClick={handleClick}>Reset</button>
+      <span>
+        {calculateWinner(squares)}
+      </span>
       <div className="container">
         {squares.map((value, index) => {
           return (
--- src/Square.jsx
+++ src/Square.jsx
@@ -27,7 +27,7 @@ const Square = (props) => {
     console.log(props.squares, props.player)
     return (
         <div className="square" onClick={handleClick}>
-            {props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
+            {props.squareValue === "O" ? <img alt='cool pic' src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
         </div>
     );
 };
